61182979432023884fe3967983aee1e9bae59b04
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-02-27 19:24:09 +0800

    Created mobile menu

---
+++ src/assets/components/Popup/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className="menu">
+        <p className="menu__item">
+            About
+        </p>
+        <div className="menu__hline"></div>
+        <p className="menu__item">
+            Discover
+        </p>
+        <div className="menu__hline"></div>
+        <p className="menu__item">
+            Get Started
+        </p>
+    </div>
+  )
+}
+
+export default Menu
