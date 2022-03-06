87f41252428e75387c50a780ce9080287d66de51
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-06 20:13:45 +0800

    Made trackbar show during page load

--- src/assets/components/Body/Tracker.js
+++ src/assets/components/Body/Tracker.js
@@ -1,22 +1,21 @@
 import React, { useEffect } from 'react'
 
 const Tracker = ({amount}) => {
-
   const showProgress = () => {
     const slider = document.querySelector(".progress__slider");
-    console.log("Max: " + slider.max);
-    console.log("Min: " + slider.min);
-    console.log("Val: " + slider.value);
     const bgColor = 'hsl(192, 29%, 97%)';
     const fillColor = 'hsl(176, 50%, 47%)';
     let val = Math.floor((slider.value - slider.min) / (slider.max - slider.min) * 100);
-    console.log("Value: " + val);
     slider.style.background = `linear-gradient(to right, 
                           ${fillColor} 0%, 
                           ${fillColor} ${val}%,
                           ${bgColor} ${val}%,
                           ${bgColor} 100%)`;
   }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    showProgress();
+  })
 
   useEffect(() => showProgress);
 
